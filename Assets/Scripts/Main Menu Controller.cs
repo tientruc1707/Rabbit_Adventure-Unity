04bfe541e3ab3976f13b5e5517760c7094db8f65
tientruc1707 <nguyentientruc2001@gmail.com>
2024-06-01 21:06:24 +0700

    Add LoadSceneScript, make Menu

--- Assets/Scripts/Main Menu Controller.cs
+++ Assets/Scripts/Main Menu Controller.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuController : MonoBehaviour
+public class SceneController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public void Menu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
     public void Play()
     {
         SceneManager.LoadScene("Select Level");
